bc15cc9e931bf51d730cc16c02a1f9fefdd5458e
Aliaksandr Miron <alex.miron1999@gmail.com>
2020-07-14 21:54:24 +0300

    implement animator base functionality

---
+++ Assets/Scenes/Animation/Scripts/AnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private Boolean isAnimating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !isAnimating)
+        {
+            animator.SetBool("isAnimating", true);
+            isAnimating = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && isAnimating)
+        {
+            animator.SetBool("isAnimating", false);
+            isAnimating = false;
+        }
+    }
+}
